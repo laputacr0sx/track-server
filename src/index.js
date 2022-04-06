4638d80e1897b4a0f8965da6937f612043bab8e6
veilicsVV <laputafelix@gmail.com>
2022-04-06 23:40:58 +0800

    D06042022T2340 tack-server good

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+require('./models/User');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hi there');
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+	'mongodb+srv://admin:adminadmin@veilicsmongodb.jtjr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+	console.log('connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+	res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
 });
